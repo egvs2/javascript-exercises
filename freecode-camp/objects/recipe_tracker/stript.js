74ad81fee95b855c92bb1f2dca701329783491d3
Emerson Gomes <emersongvs@gmail.com>
2025-09-13 17:47:35 -0300

    finalização do recipe tracker

--- freecode-camp/objects/recipe_tracker/stript.js
+++ freecode-camp/objects/recipe_tracker/stript.js
@@ -21,13 +21,13 @@ const recipe2 = {
 }
 
 const recipe3 = {
-  name: 'Vegetable Stir Fry',
-  ingredients: ['broccoli', 'carrot', 'bell pepper'],
-  cookingTime: 15,
-  totalIngredients: null,
-  difficultyLevel: '',
-  ratings: [4, 3, 4, 5],
-  averageRating: null,
+    name: 'Vegetable Stir Fry',
+    ingredients: ['broccoli', 'carrot', 'bell pepper'],
+    cookingTime: 15,
+    totalIngredients: null,
+    difficultyLevel: '',
+    ratings: [4, 3, 4, 5],
+    averageRating: null,
 };
 
 recipes.push(recipe1, recipe2, recipe3);
@@ -36,6 +36,39 @@ function getAverageRating(notas){
     return (notas[0]+ notas[1]+ notas[2]+ notas[3])/notas.length;
 };
 
+function getTotalIngredients(ingredients) {
+    return ingredients.length;
+}
+
+function getDifficultyLevel(cooktime){
+    if(cooktime <= 30){
+        return "easy";
+    }else if(cooktime <= 60){
+        return "medium";
+    }else{
+        return "hard";
+    }
+};
+
+
 console.log(getAverageRating(recipe1.ratings));
 console.log(getAverageRating(recipe2.ratings));
 console.log(getAverageRating(recipe3.ratings));
+
+let recipe1AverageRating = getAverageRating(recipe1.ratings);
+let recipe1TotalIngredients = getTotalIngredients(recipe1.ingredients);
+let recipe1DifficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+
+console.log(recipe1AverageRating);
+console.log(recipe1TotalIngredients);
+console.log(recipe1DifficultyLevel);
+
+recipe1["averageRating"] = recipe1AverageRating;
+recipe1["totalIngredients"] = recipe1TotalIngredients;
+recipe1["difficultyLevel"] = recipe1DifficultyLevel;
+
+recipe2.averageRating = getAverageRating(recipe2.ratings);
+recipe2.totalIngredients = getTotalIngredients(recipe2.ingredients)
+recipe2.difficultyLevel = getDifficultyLevel(recipe2.cookingTime)
+
+console.log(recipes);
